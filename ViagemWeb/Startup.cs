1fa150617d2e67ea8c5ebe2b774e2cbe325b977e
Diovane <diovani.joao@gmail.com>
2018-10-24 22:01:36 -0300

    Add project files.

---
+++ ViagemWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ViagemWeb.Startup))]
+namespace ViagemWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
